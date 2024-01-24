a10c9af834be9caa5d1ac6235fd1f5bc33174a0f
RBX6 <156525459+RBX6@users.noreply.github.com>
2024-01-24 22:22:14 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,13 +1,35 @@
--- Charge la bibliothèque Fluent depuis le lien fourni
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-
--- Charge le gestionnaire de sauvegarde depuis le lien fourni
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-
--- Charge le gestionnaire d'interface depuis le lien fourni
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+--Icons https://lucide.dev/icons/ 
+SaveManager:LoadAutoloadConfig()
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
 
--- Crée une fenêtre avec Fluent
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+Window:SelectTab(1)
+local Tabs = {
+    Dashboard = Window:AddTab({ Title = "Dashboard", Icon = "layout-grid" }),
+    GameScripts = Window:AddTab({ Title = "Games Scripts", Icon = "file-code" }),
+    UniScripts = Window:AddTab({ Title = "Universal Script", Icon = "book-open-text" }),
+    Credits = Window:AddTab({ Title = "Credits", Icon = "info" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+local Options = Fluent.Options
 local Window = Fluent:CreateWindow({
     Title = "Guard Hub " .. Fluent.Version,
     SubTitle = "by RBX6",
@@ -17,88 +39,68 @@ local Window = Fluent:CreateWindow({
     Theme = "Dark",
     MinimizeKey = Enum.KeyCode.Z
 })
+do
+    Fluent:Notify({
+        Title = "Guard Hub",
+        Content = "Bienvenue à Guard Hub!",
+        SubContent = "Créer par RBX6",
+        Duration = 2
+    })
 
--- Icons https://lucide.dev/icons/
-local Tabs = {
-    Dashboard = Window:AddTab({ Title = "Dashboard", Icon = "layout-grid" }),
-    GameScripts = Window:AddTab({ Title = "Games Scripts", Icon = "file-code" }),
-    UniScripts = Window:AddTab({ Title = "Universal Script", Icon = "file-code2" }),
-    Credits = Window:AddTab({ Title = "Credits", Icon = "Credits" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
+    Tabs.Dashboard:AddParagraph({
+        Title = "Bienvenue",
+        Content = "Bienvenue Sur Guard Hub Lien Officiel : discord.gg/hrshopp"
+    })
 
--- Options Fluent
-local Options = Fluent.Options
+    Tabs.Credits:AddParagraph({
+        Title = "CREDITS",
+        Content = "FONDATEUR : RBX6 | BETA TESTEUR : NETABEATS, MISTERNUT"
+    })
 
--- Notification de bienvenue
-Fluent:Notify({
-    Title = "Guard Hub",
-    Content = "Bienvenue à Guard Hub!",
-    SubContent = "Créé par RBX6",
-    Duration = 2
-})
-
--- Ajoute un paragraphe dans l'onglet Dashboard
-Tabs.Dashboard:AddParagraph({
-    Title = "Bienvenue",
-    Content = "Bienvenue Sur Guard Hub Lien Officiel : discord.gg/hrshopp"
-})
-
--- Ajoute un paragraphe dans l'onglet Credits
-Tabs.Credits:AddParagraph({
-    Title = "CREDITS",
-    Content = "FONDATEUR : RBX6 | BETA TESTEUR : NETABEATS, MISTERNUT"
-})
-
--- Ajoute un bouton dans l'onglet Dashboard pour rejoindre le Discord
-Tabs.Dashboard:AddButton({
-    Title = "Rejoindre le Discord",
-    Description = "Permet de Rejoindre notre Serveur Discord",
-    Callback = function()
-        Window:Dialog({
-            Title = "GuardHub | Information",
-            Content = "Voulez-Vous Rejoindre Notre Discord ?",
-            Buttons = {
-                {
-                    Title = "Oui",
-                    Callback = function()
-                        setclipboard("discord.gg/hrshopp")
-                    end
-                },
-                {
-                    Title = "Non",
-                    Callback = function()
-                    end
+    Tabs.Dashboard:AddButton({
+        Title = "Rejoindre le Discord",
+        Description = "Permet de Rejoindre notre Serveur Discord",
+        Callback = function()
+            Window:Dialog({
+                Title = "GuardHub | Information",
+                Content = "Voulez-Vous Rejoindre Notre Discord ?",
+                Buttons = {
+                    {
+                        Title = "Oui",
+                        Callback = function()
+                            setclipboard("discord.gg/hrshopp")
+                        end
+                    },
+                    {
+                        Title = "Non",
+                        Callback = function()
+                        end
+                    }
                 }
-            }
-        })
-    end
-})
+            })
+        end
+    })
 
--- Ajoute un bouton dans l'onglet Dashboard pour exécuter le script "Survive The Killer"
-Tabs.Dashboard:AddButton({
-    Title = "Survive The Killer",
-    Description = "Permet d'Exécuter le Script Survive the Killer",
-    Callback = function()
-        Window:Dialog({
-            Title = "GuardHub | Information",
-            Content = "Voulez-vous exécuter le Script?",
-            Buttons = {
-                {
-                    Title = "Oui",
-                    Callback = function()
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/main.lua"))()
-                    end
-                },
-                {
-                    Title = "Non",
-                    Callback = function()
-                    end
+    Tabs.GameScripts:AddButton({
+        Title = "Survive The Killer",
+        Description = "Permet de Executer le Script Survive the Killer",
+        Callback = function()
+            Window:Dialog({
+                Title = "GuardHub | Information",
+                Content = "Voulez-vous éxecuter le Script?",
+                Buttons = {
+                    {
+                        Title = "Oui",
+                        Callback = function()
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/Milan08Studio/ChairWare/main/main.lua"))()
+                        end
+                    },
+                    {
+                        Title = "Non",
+                        Callback = function()
+                        end
+                    }
                 }
-            }
-        })
-    end
-})
-
--- Charge la configuration automatique si disponible
-SaveManager:LoadAutoloadConfig()
+            })
+        end
+    })
